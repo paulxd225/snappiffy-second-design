16383404b40c7ecfaa621297a14c6a034f9f6b60
Dev.Paul Paez <paulpaeztenecota@gmail.com>
2026-04-20 10:36:03 -0500

    feat: :sparkles: errores de importacion
    se corrigio el error de imnportacion de react

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,7 +1,10 @@
+
 import { useEffect, useState } from 'react';
+
 import { Icons } from '../components/Icons';
 import { useReveal } from '../hooks/use-reveal';
 import { useScrollDirection } from '../hooks/use-scroll-direction';
+
 
 
 const logos = [
